d22a0529b3a63d1cf922290764daf26fecd60e20
Mac User <MacUser@Mac-Users-MacBook-8.local>
2016-08-14 15:11:42 -0400

    Conditional alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,9 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else {
+	alert("Try again!");
+}
