e5f46e2ca064af3af8a662e8399535c1d0bc5841
Mac User <MacUser@Mac-Users-MacBook-8.local>
2016-08-15 03:42:00 -0400

    Add createboard logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,9 +3,17 @@ var cardOne = "king";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "queen";
-if (cardOne === cardThree) {
+/*if (cardOne === cardThree) {
 	alert("You found a match!");
 }
 else {
 	alert("Try again!");
+}*/
+var createBoard = function() {
+	var x = document.getElementById('gameboard');
+	for (n = 0; n < 4; n++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		x.appendChild(card);
+	}
 }
